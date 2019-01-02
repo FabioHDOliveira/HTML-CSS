30fb674fd5a15f460dec161f4ca8a4f59e116aa5
FabioHDOliveira <39977897+FabioHDOliveira@users.noreply.github.com>
2019-01-02 11:09:12 -0800

    Add files via upload

---
+++ Dons_Cafe/coffees.js
@@ -0,0 +1,11 @@
+                    
+    var blueMountain = "This extraordinary coffee, famous " + "for its exquisite flavor and strong body, " + "is grown in the majestic Blue Mountain range in Jamaica.";
+                    
+    var blueGrove = "This delightful coffee has an aroma " + "that is captivatingly rich and nutty " + "with a faint hint of citrus.";
+
+    var sumatra = "One of the finest coffees in the world, " + "medium roasted to accentuate its robust character."
+                    
+    var kona = "Grown and processed using traditional Hawaiian "+ "methods, then roasted in small batches to maintain "+ "peak freshness and flavor."
+                    
+    var antigua = "An outstanding coffee with a rich, spicy, and smokey flavor."
+                    
